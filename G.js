2010e1c36e10a20811314548c46ee4c498293890
L-Stewart <stewartalorin@gmail.com>
2021-02-01 21:32:44 -0800

    added some starter files for equations and code for the gravity equation

---
+++ G.js
@@ -0,0 +1,4 @@
+// g = -GM/r(2)
+// g- gravity, G- gravitional constant, M- mass of large object, r- radius of large object, ()- exponent, P.S large object = Earth
+
+// g = -(1 * (5.9722 * (10 ** 24))) / (3958.8 ** 2)
